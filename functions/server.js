282dc81195215ddada690d3b0536e83d92efb1f4
Muponisi Seviour Novela <69425152+benwhiji@users.noreply.github.com>
2024-07-06 06:15:51 +0200

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -231,6 +231,8 @@ const matchResults = [
         "Challenger": "Kaizer Chiefs",
         "opponent": "Mamelodi Sundowns",
         "score": "1-5",
+        "opp_url": "https://tmssl.akamaized.net/images/wappen/head/6356.png?lm=1611130082",
+        "cha_url":"https://tmssl.akamaized.net/images/wappen/head/568.png?lm=1404764408",
         "highlights_url": "https://za.soccerway.com/teams/south-africa/kaizer-chiefs/3528/matches/"
     },
     {
@@ -238,6 +240,8 @@ const matchResults = [
         "Challenger":"Kaizer Chiefs",
         "opponent": "TS Galaxy",
         "score": "2-2",
+        "cha_url":"https://tmssl.akamaized.net/images/wappen/head/568.png?lm=1404764408",
+        "opp_url": "https://tmssl.akamaized.net/images/wappen/head/67074.png?lm=1543132402",
         "highlights_url": "https://za.soccerway.com/teams/south-africa/kaizer-chiefs/3528/matches/"
     },
     {
@@ -245,6 +249,8 @@ const matchResults = [
         "Challenger":"Orlando Pirates",
         "opponent": "Kaizer Chiefs",
         "score": "3-1",
+        "cha_url": "https://tmssl.akamaized.net/images/wappen/head/2557.png?lm=1474757243",
+        "opp_url":"https://tmssl.akamaized.net/images/wappen/head/568.png?lm=1404764408",
         "highlights_url": "https://www.besoccer.com/team/matches/kaizer-chiefs"
     },
     {
@@ -252,6 +258,8 @@ const matchResults = [
         "Challenger":"Kaizer Chiefs",
         "opponent": "Cape Town City",
         "score": "0-0",
+        "cha_url":"https://tmssl.akamaized.net/images/wappen/head/568.png?lm=1404764408",
+        "opp_url": "https://tmssl.akamaized.net/images/wappen/head/2303.png?lm=1488051605",
         "highlights_url": "https://www.besoccer.com/team/matches/kaizer-chiefs"
     },
     {
@@ -259,6 +267,8 @@ const matchResults = [
         "Challenger":"Kaizer Chiefs",
         "opponent": "AmaZulu",
         "score": "2-1",
+        "cha_url":"https://tmssl.akamaized.net/images/wappen/head/568.png?lm=1404764408",
+        "opp_url": "https://tmssl.akamaized.net/images/wappen/head/9370.png?lm=1499719939",
         "highlights_url": "https://www.goal.com/en-za/team/kaizer-chiefs/fixtures-results/9g72y015b6fgkgtpx1c67qemi"
     },
     {
@@ -266,6 +276,9 @@ const matchResults = [
         "Challenger":"Kaizer Chiefs",
         "opponent": "SuperSport United",
         "score": "1-1",
+        
+        "cha_url":"https://tmssl.akamaized.net/images/wappen/head/568.png?lm=1404764408",
+        "opp_url": "https://tmssl.akamaized.net/images/wappen/head/2891.png?lm=1474825244",
         "highlights_url": "https://www.goal.com/en-za/team/kaizer-chiefs/fixtures-results/9g72y015b6fgkgtpx1c67qemi"
     }
 
